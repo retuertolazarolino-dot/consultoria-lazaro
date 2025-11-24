3ce2f77f9ad416cbac51dcf384fc18c68b6f9df9
Lialrela <retuertolazarolino@gmail.com>
2025-11-23 21:07:33 -0500

    Fuerzo nuevo deploy

---
+++ app/servicios/levantamientos-topograficos/page.tsx
@@ -0,0 +1,8 @@
+export default function LevantamientosTopograficos() {
+  return (
+    <div>
+      <h1>Levantamientos Topográficos</h1>
+      <p>Contenido en construcción jaja.</p>
+    </div>
+  );
+}
